1858a3cfe76a5e6aca953188b0a00a717dd90f88
Shan Wu <shan.wu@taxify.eu>
2018-12-03 14:04:49 +0200

    minor updates to version v1.5
    * base function works fine

--- EMTA/check_room.py
+++ EMTA/check_room.py
@@ -1,3 +1,4 @@
+# configure path: '/Users/simonwu/Library/Application Support/telegram-send'
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import ssl
@@ -7,7 +8,7 @@ import multiprocessing as mp
 
 
 def run_fetcher():
-    print('Updating soup...')
+    print('> DEBUG: Updating soup...')
     url = 'https://sise.ema.edu.ee/vaatleja/vabadruumid2.x'
     soup = BeautifulSoup(make_request(url, context=ssl.SSLContext(ssl.PROTOCOL_TLSv1)), features="lxml")
     return soup
@@ -20,7 +21,6 @@ def make_request(url, context=None):
     return html
 
 def find_waiting_list(soup, name=None):
-    print('Finding your name in the list...')
     items = soup.find_all(color='black')
     str_calling_list_est = items[0].b.text
     str_waiting_list_est = items[1].text
@@ -54,7 +54,6 @@ def find_waiting_list(soup, name=None):
             position = count
 
         elif 'inimest' in each_name:
-            print('Number of waiting list: ' + each_name[:2])
             total_number = int(each_name[:2])
             break
         else:
@@ -62,16 +61,15 @@ def find_waiting_list(soup, name=None):
 
     return status, position, total_number
 
-def run(name):
-    soup = run_fetcher()
-    status, position, num = find_waiting_list(soup, name)
-    return status, position, num
-
-def run_forever(name, debug=False, interval=30):
+def run_spy(name, debug=False, interval=30, init=3):
     try:
-        print('Auto bot start running...')
+        print('> DEBUG: Hello! %s' % name)
+        print('> DEBUG: Auto bot start running...')
         if not debug:
-            telegram_send.send(messages=['Hello, %s. You will be notified when there are updates in the queue.' % name])
+            telegram_send.send(messages=['Hello, %s. You will be notified when there are updates in the queue.' % name],
+                               conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-xun.conf')
+            telegram_send.send(messages=['Hello, %s. You will be notified when there are updates in the queue.' % name],
+                               conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-shan.conf')
 
         # init
         wait_position = -1
@@ -79,50 +77,84 @@ def run_forever(name, debug=False, interval=30):
         status = -1
         send_call = True
         send_wait = True
+        count_init = 0
 
         while True:
-            status, position, num = run(name)
+            soup = run_fetcher()
+            status, position, num = find_waiting_list(soup, name)
 
             # check status
             if status == -1:
-                print('You are not in the list!')
+                print('> INFO: You are not in the list!')
+                count_init += 1
+                if count_init == init:
+                    print("Auto shut down.")
+                    raise KeyboardInterrupt
 
             elif status == 0:
-                print('Your position in waiting list: %d' % position)
+                print('> INFO: You are in the waiting queue: %d/%d' % (position, num))
                 if wait_position != position:
                     send_wait = True
                     wait_position = position
 
                 if send_wait and not debug:
-                    telegram_send.send(messages=['You are in the waiting queue: %d/%d' % (position, num)])
+                    telegram_send.send(messages=['You are in the waiting queue: %d/%d' % (position, num)],
+                                       conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-xun.conf')
+                    telegram_send.send(messages=['You are in the waiting queue: %d/%d' % (position, num)],
+                                       conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-shan.conf')
                     send_wait = False
 
             elif status == 1:
-                print('Your position in calling list: %d' % position)
+                print('> INFO: Your position in calling list: %d' % position)
                 if call_position != position:
                     send_call = True
                     call_position = position
 
                 if send_call and not debug:
-                    telegram_send.send(messages=['You can go to pick up your key now!', 'position: %d.' % position])
+                    telegram_send.send(messages=['You can go to pick up your key now!', 'position: %d.' % position],
+                                       conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-xun.conf')
+                    telegram_send.send(messages=['You can go to pick up your key now!', 'position: %d.' % position],
+                                       conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-shan.conf')
                     send_call = False
 
             time.sleep(interval)
     except KeyboardInterrupt:
-        print('bye:)')
+        print('> DEBUG: bye:)')
+        if not debug:
+            telegram_send.send(messages=['Bot has been shut down. Bye:)'],
+                               conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-xun.conf')
+            telegram_send.send(messages=['Bot has been shut down. Bye:)'],
+                               conf='/Users/simonwu/PycharmProjects/life_utils/EMTA/config/telegram-send-shan.conf')
     return
 
 class ClassRoom_Bot():
     def __init__(self, name):
         self.name = name
         self.records = {}  # time: status
-        self.threads = []
+        self.threads = {}
+
+        # init user promt
+        p_usr = mp.Process(target=self.usr_interface, args=(self,), name='usr')
+        self.threads['usr'] = p_usr
+        p_usr.start()
         return
 
     def run_server(self):
-        p1 = mp.Process(target=run_forever, args=(self.name,), name='server_bot')
-        self.threads.append(p)
-        p2 = mp.Process(target=self.usr_interface, args=(self,), name='usr')
+        return
+
+    def init_server(self):
+        return
+
+    def init_new_spy(self, name):
+        t_name = 'spy_%s' % self.name
+        p = mp.Process(target=run_spy, args=(self.name,), name=t_name)
+        self.threads[t_name] = p
+        p.start()
+        return
+
+    def terminate_spy(self, t_name):
+        p = self.threads[t_name]
+        # send sigkill
         return
 
     def usr_interface(self):
